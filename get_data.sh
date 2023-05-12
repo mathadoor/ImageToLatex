d95063783f068c5fab1a885ff779531b868805d8
mathadoor <matharooh2@gmail.com>
2023-05-11 22:48:27 -0400

    setting up data download pipeline

---
+++ get_data.sh
@@ -0,0 +1,22 @@
+# download CROHME Dataset to the data folder in the current directory
+# If the data already exists, do not download again
+# Usage: bash get_data.sh
+if [ -d data/ICFHR_package ]; then
+    echo "data already exists, cleaning up the directory"
+    rm -rf data/ICFHR_package
+fi
+if [ -f data/ICFHR_package.zip ]; then
+    echo "Would you like to redownload?"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes )
+              rm data/ICFHR_package.zip;
+              wget http://www.isical.ac.in/~crohme/ICFHR_package.zip -P data;
+              break;;
+            No )
+              echo "Using the existing zip file";;
+        esac
+    done
+    else wget http://www.isical.ac.in/~crohme/ICFHR_package.zip -P data
+fi
+unzip data/ICFHR_package.zip -d data && rm data/ICFHR_package.zip
