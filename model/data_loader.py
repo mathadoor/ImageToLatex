4b812176497911c28a0442898b1878012253ad11
mathadoor <matharooh2@gmail.com>
2023-05-25 18:34:42 -0400

    Added skeleton for the dataloader

---
+++ model/data_loader.py
@@ -0,0 +1,7 @@
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+# Going to have to import the inkML files instead to accomodate the data.
+# I think I might as well create different classes pertaining to stroke data. But that is not important at this point
+
+# TODO: Create a class for the dataset
