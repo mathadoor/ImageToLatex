65d7237207a93dd5934fe50c564e5cf09804fc5f
mathadoor <matharooh2@gmail.com>
2023-05-25 16:46:28 -0400

    Added one configuration for model. And training and modeling classes

--- model/models.py
+++ model/models.py
@@ -1,9 +1,20 @@
 # Define the model architectures here
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
 
+BASE_CONFIG = {
+    'num_layers': 3,
+    'layer_dim_config': [32, 64, 128],
+    'kernel_size': 3,
+    'num_classes': 10,
+    'DEVICE': torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
+}
 class baseWatcher:
-    def __init__(self, model, config):
-        self.model = model
-        self.config = config
+    def __init__(self, config):
+        if not config:
+            self.config = BASE_CONFIG
+
 
     def train(self):
         pass
@@ -32,9 +43,25 @@ class baseParser:
 
     def load(self):
         pass
+
 class VanilleCNNWatcher(baseWatcher):
-    def __init__(self, model, config):
-        super().__init__(model, config)
+    def __init__(self, config):
+        super().__init__(config)
+        self.generate_model()
+
+    def generate_model(self):
+        '''
+        Generate the model based on the config
+        :return: None
+        '''
+        self.model = nn.Sequential()
+        layer_dims = self.config['layer_dim_config'] + [self.config['num_classes']]
+        kernel_size = self.config['kernel_size']
+        for i in range(self.config['num_layers'] + 1):
+            self.model.add_module('conv{}'.format(i), nn.Conv2d(layer_dims[i], layer_dims[i+1], kernel_size))
+            self.model.add_module('relu{}'.format(i), nn.ReLU())
+            self.model.add_module('maxpool{}'.format(i), nn.MaxPool2d(2, 2))
+        self.model.to(self.config['DEVICE'])
 
     def train(self):
         pass
