31f9b61c6ea383009a54e4cbcf72f57299130655
mathadoor <matharooh2@gmail.com>
2023-05-26 23:06:24 -0400

    update data_loader.py

--- model/data_loader.py
+++ model/data_loader.py
@@ -4,4 +4,26 @@ from torchvision import transforms
 # Going to have to import the inkML files instead to accomodate the data.
 # I think I might as well create different classes pertaining to stroke data. But that is not important at this point
 
-# TODO: Create a class for the dataset
+# Custom Class to load the data into dataloader
+class StrokeData(Dataset):
+
+    def __init__(self, path, transform=None):
+        self.path = path
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.path)
+
+    def __getitem__(self, idx):
+
+        # Load the data from the path
+        data = np.load(self.path[idx])
+
+        # Get the label from the path
+        label = self.path[idx].split('/')[-1].split('.')[0]
+
+        # Transform the data
+        if self.transform:
+            data = self.transform(data)
+
+        return data, label
